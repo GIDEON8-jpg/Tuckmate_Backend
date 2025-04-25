103c79c5928511bc95626cb1479f604fccf7e151
Gideon Zimano <munashezimano@gmail.com>
2025-04-25 13:14:53 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,56 +1,56 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
 const bcrypt = require('bcryptjs');
 
-const User = sequelize.define('User', {
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
     username: {
-        type: DataTypes.STRING(50),
-        unique: true,
-        allowNull: false
+      type: DataTypes.STRING(50),
+      unique: true,
+      allowNull: false
     },
     email: {
-        type: DataTypes.STRING(100),
-        unique: true,
-        allowNull: false,
-        validate: {
-            isEmail: true
-        }
+      type: DataTypes.STRING(100),
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        set(value) {
-            const salt = bcrypt.genSaltSync(10);
-            this.setDataValue('password', bcrypt.hashSync(value, salt));
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value) {
+        const salt = bcrypt.genSaltSync(10);
+        this.setDataValue('password', bcrypt.hashSync(value, salt));
+      }
     },
     full_name: {
-        type: DataTypes.STRING(100)
+      type: DataTypes.STRING(100)
     },
     phone: {
-        type: DataTypes.STRING(20),
-        validate: {
-            is: /^(\+?\d{1,3}[- ]?)?\d{10}$/
-        }
+      type: DataTypes.STRING(20),
+      validate: {
+        is: /^(\+?\d{1,3}[- ]?)?\d{10}$/
+      }
     },
     role: {
-        type: DataTypes.ENUM('admin', 'customer'),
-        defaultValue: 'customer'
+      type: DataTypes.ENUM('admin', 'customer'),
+      defaultValue: 'customer'
     }
-}, {
+  }, {
     timestamps: true,
     underscored: true,
     tableName: 'users'
-});
+  });
 
-// Instance method for password verification
-User.prototype.verifyPassword = function(password) {
+  // Add instance method
+  User.prototype.verifyPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
-};
+  };
 
-module.exports = User;
+  return User;
+};
